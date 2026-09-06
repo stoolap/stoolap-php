e04c313aeaaba20621c919b1d016806934e0c99f
semihalev <semihalev@gmail.com>
2026-09-07 00:06:45 +0300

    Bump version to 0.4.1 for stoolap engine v0.4.1

--- ext/php_stoolap.h
+++ ext/php_stoolap.h
@@ -4,7 +4,7 @@
 extern zend_module_entry stoolap_module_entry;
 #define phpext_stoolap_ptr &stoolap_module_entry
 
-#define PHP_STOOLAP_VERSION "0.4.0"
+#define PHP_STOOLAP_VERSION "0.4.1"
 
 /* Stoolap C API types */
 typedef struct StoolapDB StoolapDB;
